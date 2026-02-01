1b29640db6ec64c927e2cf122d9f31369184ba46
cother <cother@protonmail.com>
2026-02-01 17:49:41 -0600

    initial config with home-manager and all the mandanga

---
+++ configuration.nix
@@ -0,0 +1,106 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  # boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+    useOSProber = true;
+  };
+
+  networking.hostName = "nixos-btw";
+  # networking.wireless.enable = true;
+  networking.networkmanager.enable = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Set your time zone.
+  time.timeZone = "America/Mexico_City";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_MX.UTF-8";
+    LC_IDENTIFICATION = "es_MX.UTF-8";
+    LC_MEASUREMENT = "es_MX.UTF-8";
+    LC_MONETARY = "es_MX.UTF-8";
+    LC_NAME = "es_MX.UTF-8";
+    LC_NUMERIC = "es_MX.UTF-8";
+    LC_PAPER = "es_MX.UTF-8";
+    LC_TELEPHONE = "es_MX.UTF-8";
+    LC_TIME = "es_MX.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "latam";
+    variant = "";
+  };
+  services.getty.autologinUser = "cother"; # niputaidea
+
+  # Configure console keymap
+  console.keyMap = "la-latin1";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.cother = {
+    isNormalUser = true;
+    description = "cother";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    kitty
+    hyprpaper
+    brave
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
---
+++ home.nix
@@ -0,0 +1,38 @@
+{config, pkgs, ...}: {
+  home.username = "cother";
+  home.homeDirectory = "/home/cother";
+  home.stateVersion = "25.11";
+  home.packages = with pkgs; [
+    wl-clipboard
+    cliphist
+  ];
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo I use nixos, btw";
+    };
+    profileExtra = ''
+      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+        exec hyprland
+      fi
+    '';
+  };
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    extraLuaConfig = ''
+      vim.opt.clipboard =  "unnamedplus"
+    '';
+  };
+  programs.git = {
+    enable = true;
+    userName = "cother";
+    userEmail = "cother@protonmail.com";
+    extraConfig =  {
+      init.defaultBranch = "main";
+      core.editor = "nvim";
+    };
+  };
+  home.file.".config/hypr".source = ./config/hypr;
+}
