84f3037f37f27d24dcef9ac7f3c1823c6c7f5bb5
cother <cother@protonmail.com>
2026-02-08 10:12:53 -0600

    dark mode?

--- home.nix
+++ home.nix
@@ -54,4 +54,14 @@
   };
   home.file.".config/hypr".source = ./config/hypr;
   home.file.".config/waybar".source = ./config/waybar;
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+    };
+    iconTheme = {
+      name = "Adwaita";
+    };
+  };
 }
