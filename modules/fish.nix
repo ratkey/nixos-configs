27c28f36caaad8cf2ee3177e48addb67de7da2c9
cother <cother@protonmail.com>
2026-02-08 14:02:26 -0600

    sylize fish vim modes

--- modules/fish.nix
+++ modules/fish.nix
@@ -5,6 +5,28 @@
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
       fish_vi_key_bindings
+
+      function fish_mode_prompt
+        switch $fish_bind_mode
+          case default
+            set_color --bold --background fe8019 282828
+            echo ' N '
+            set_color normal
+          case insert
+            set_color --bold --background b8bb26 282828
+            echo ' I '
+            set_color normal
+          case visual
+            set_color --bold --background fabd2f 282828
+            echo ' V '
+            set_color normal
+          case replace_one
+            set_color --bold --background d3869b 282828
+            echo ' R '
+            set_color normal
+        end
+        echo -n " "
+      end
     '';
     shellAliases = {
       btw = "echo I use nixos, btw";
