6188bb8f9c317cf86865513e078389d2cbd1d770
cother <cother@protonmail.com>
2026-02-19 09:28:38 -0600

    fix: updatee masson repo

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,29 +1,9 @@
 { pkgs, ... }:
-let
-  installLazyVim = pkgs.writeShellScriptBin "install-lazyvim" ''
-    #!/usr/bin/env bash
-    
-    NVIM_CONFIG="$HOME/.config/nvim"
-    
-    if [ -d "$NVIM_CONFIG" ]; then
-      BACKUP_DIR="$NVIM_CONFIG.bak.$(date +%Y%m%d_%H%M%S)"
-      echo "Backing up existing nvim config to $BACKUP_DIR..."
-      mv "$NVIM_CONFIG" "$BACKUP_DIR"
-    fi
-
-    echo "Cloning LazyVim starter..."
-    git clone https://github.com/LazyVim/starter "$NVIM_CONFIG"
-
-    # Remove the .git folder so you can add it to your own repo later
-    rm -rf "$NVIM_CONFIG/.git"
-
-    echo "LazyVim installed! Run 'nvim' to start."
-  '';
-in
 {
+  # Link the declarative config from your repository
+  xdg.configFile."nvim".source = ../config/nvim;
+
   home.packages = with pkgs; [
-    installLazyVim
-    
     # Core tools often needed by LazyVim/Mason external to nvim wrapper
     ripgrep
     fd
